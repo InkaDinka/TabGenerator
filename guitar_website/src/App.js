7902dd691e03ddfd6776c6d8c59d35ad66d78fe0
Jared <72281577+jachiaram@users.noreply.github.com>
2024-07-19 20:23:26 +0200

    Got a basic pdf viewer set up.

--- guitar_website/src/App.js
+++ guitar_website/src/App.js
@@ -5,20 +5,7 @@ import PdfDownload from './components/PdfDownload';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <PdfDownload/>
-        </a>
-      </header>
     </div>
   );
 }
--- guitar_website/src/components/PdfDownload.js
+++ guitar_website/src/components/PdfDownload.js
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+import document from '../document.pdf';
+
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
+
 function PdfDownload() {
-return (
+    const [numPages, setNumPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	const onDocumentLoadSuccess = ({ numPages }) => {
+		setNumPages(numPages);
+	};
+
+	const goToPrevPage = () =>
+		setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
+
+	const goToNextPage = () =>
+		setPageNumber(
+			pageNumber + 1 >= numPages ? numPages : pageNumber + 1,
+		);
+
+    return (
     <div>
-        <p>This is a test.</p>
+        <nav>
+            <button onClick={goToPrevPage}>Prev</button>
+            <button onClick={goToNextPage}>Next</button>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
+        </nav>
+
+        <Document
+            file={document}
+            onLoadSuccess={onDocumentLoadSuccess}
+            >
+                <Page pageNumber={pageNumber} />
+            </Document>
     </div>
     );
 }
